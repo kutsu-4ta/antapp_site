61b94984c16d9c3d03f16964493c1cc8303fa51f
kutsu-4ta <yamashita.bus@gmial.com>
2022-06-26 12:15:00 +0900

    PC端末 画面幅スクロールバーの幅考慮

--- antapp/src/components/backgrounAnimation/Triangle.tsx
+++ antapp/src/components/backgrounAnimation/Triangle.tsx
@@ -22,7 +22,7 @@ export const UseTriangleAnimation: UseTriangleAnimation = () => {
                 border-right: 0px solid transparent;
               }
               100% {
-                   border-right: ${window.innerWidth + "px"} solid transparent;
+                   border-right: ${window.innerWidth -20 + "px"} solid transparent;
               }
             `
         );
@@ -31,7 +31,7 @@ export const UseTriangleAnimation: UseTriangleAnimation = () => {
     const Triangle = styled.div`
       border-top: ${() => window.innerHeight * 0.5 + "px"} solid #f2ff54; /* 高さ */
       border-bottom: ${() => window.innerHeight - window.innerHeight * 0.5 + "px"} solid #f2ff54; /* 高さ */
-      border-right: ${() => window.innerWidth + "px"} solid transparent;
+      border-right: ${() => window.innerWidth - 20 + "px"} solid transparent; /* スクロールバーの幅20px引いてる */
       animation: ${() => TriangleAnimation} 500ms ease-in;
       //background-color: #4f5045;
 
